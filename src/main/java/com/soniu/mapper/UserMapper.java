1ffc538c143481779349470899ce23906ab2ef26
ywhy18 <ywhy18@gmail.com>
2020-03-05 14:54:02 +0900

    메인페이지 마크업

--- src/main/java/com/soniu/mapper/UserMapper.java
+++ src/main/java/com/soniu/mapper/UserMapper.java
@@ -1,6 +1,8 @@
 package com.soniu.mapper;
 
 import com.soniu.domain.userInfo_VO;
+import com.soniu.domain.userInfoAuth_VO;
+import com.soniu.domain.userPrefer_VO;
 
 public interface UserMapper {
 	public userInfo_VO read();
