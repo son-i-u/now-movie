17fd037dff73fb18a242a9e4450335e8fa50536a
sanghaklee <pirotluca@gmail.com>
2020-03-17 14:56:58 +0900

    회원가입 고칠거 개많음

--- src/main/java/com/soniu/controller/UserController.java
+++ src/main/java/com/soniu/controller/UserController.java
@@ -1,10 +1,16 @@
 package com.soniu.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.soniu.domain.userInfo_VO;
+import com.soniu.service.UserService;
 
 import lombok.extern.log4j.Log4j;
 
@@ -13,11 +19,40 @@ import lombok.extern.log4j.Log4j;
 @RequestMapping("/user/*")
 public class UserController {
 
-	/* »óÇÐ */
+	@Autowired
+	UserService userService;
+	
+	/* ï¿½ï¿½ï¿½ï¿½ */
 	@GetMapping("/register")
 	public void accessDenied(Authentication auth, Model model) {
 		log.info("register called..");
 
+	}
+	
+	/* sh */
+	@GetMapping("/register_test")
+	public void registerTest(Authentication auth, Model model) {
+		log.info("register test called..");
+
+	}
+	
+	/* sh */
+	@PostMapping("/register_test")
+	public String registerPostTest(userInfo_VO uv) {
+		log.info("registerPostTest called_....");
+		
+		userService.joinUser(uv);
+		
+		return "redirect:/movie/select/";
+	}
+	
+		
+	@PostMapping("/register")
+	public String registerPost(userInfo_VO uv) {
+		log.info("registerPost called_....");
+		userService.joinUser(uv);
+		
+		return "redirect:/movie/select/";
 	}
 	
 
